f36c94b7b5ac6ae7ddb800fc3b151633d28d24c0
Thaninpat <thaninpat.o@gmail.com>
2021-02-05 10:49:57 +0700

    Add Link

---
+++ pages/new-page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+const newPage = () => {
+  return (
+    <div>
+      <Link href='/'>back to home page</Link>
+    </div>
+  )
+}
+
+export default newPage
