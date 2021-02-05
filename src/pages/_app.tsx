432da1b2e0d9103a792ba464de2e92244bb4ea56
Thaninpat <thaninpat.o@gmail.com>
2021-02-05 15:50:09 +0700

    Update version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,15 @@
 import { AppProps } from 'next/app'
-
-// import { createClient, Provider } from 'urql'
-// import { ThemeProvider } from '@emotion/react'
 import '../styles/globals.css'
-import { ApolloProvider } from '@apollo/client'
-import { client } from '../apollo/client'
-import AuthContextProvider from '../context/AuthContextProvider'
-// import '../styles/globals.css'
 import NavBar from '../components/NavBar'
-// import theme from '../theme'
 import { ChakraProvider, CSSReset } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <ChakraProvider>
-        <CSSReset />
-        <AuthContextProvider>
-          <NavBar />
-          <Component {...pageProps} />
-        </AuthContextProvider>
-      </ChakraProvider>
-    </ApolloProvider>
+    <ChakraProvider>
+      <CSSReset />
+      <NavBar />
+      <Component {...pageProps} />
+    </ChakraProvider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,11 @@
-import Router from 'next/router'
-import { Box, Flex, Heading, Text } from '@chakra-ui/react'
-
-import { useContext, useEffect } from 'react'
-import NavBar from '../components/NavBar'
-import { AuthContext } from '../context/AuthContextProvider'
-// import Loader from 'react-spinners/ScaleLoader'
-import Loader from 'react-loader-spinner'
+import { Flex } from '@chakra-ui/react'
 import Link from 'next/link'
 
 const Index = () => {
-  const { loggedInUser } = useContext(AuthContext)
-
-  useEffect(() => {
-    if (!loggedInUser) {
-      Router.push('/')
-    }
-  }, [loggedInUser])
-
-  return !loggedInUser ? (
+  return (
     <Flex align={'center'} justify={'center'} p={20}>
       <Link href='/new-page'>New Page &rarr;</Link>
-      <Loader
-        type='ThreeDots'
-        color='#0366d6'
-        height={50}
-        width={50}
-        timeout={10000}
-      />
     </Flex>
-  ) : (
-    <>
-      {/* <NavBar /> */}
-      <Flex align={'center'} justify={'center'} p={20} flexDirection={'column'}>
-        <Heading>Hello </Heading>
-        <Text fontSize='3xl'>Home page</Text>
-        <Box>
-          <p> Username : {loggedInUser?.username}</p>
-        </Box>
-      </Flex>
-    </>
   )
 }
 
